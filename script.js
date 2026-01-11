08a7854f39bece8d2b9c3608835c706af6fb94c1
VivekMore1011 <vivekmore1011@gmail.com>
2026-01-11 19:50:33 +0530

    msg

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-let marks_
+let marks=[10,20,30,40,50];
+console.log(marks);
+
+console.log(marks[5]);
+console.log(marks.length);
+
+let heroes=["ironman","spiderman","thor","hulk"];
+console.log(heroes);
