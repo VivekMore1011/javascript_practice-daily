0b90ccc379eda98b3d2f9c222afde9d60deaa52f
VivekMore1011 <vivekmore1011@gmail.com>
2026-01-12 19:12:58 +0530

    new data added

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-let marks=[10,20,30,40,50];
-console.log(marks);
+// let marks=[10,20,30,40,50];
+// console.log(marks);
 
-console.log(marks[5]);
-console.log(marks.length);
+// console.log(marks[5]);
+// console.log(marks.length);
 
-let heroes=["ironman","spiderman","thor","hulk"];
-console.log(heroes);
+// let heroes=["ironman","spiderman","thor","hulk"];
+// console.log(heroes);
+
+// var number1=10;
+// var number2=20;
+// console.log(number1+number2);
+
