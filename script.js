883a2c04653d9b130ec747f5efc93104f8891de7
VivekMore1011 <vivekmore1011@gmail.com>
2026-01-13 19:53:24 +0530

    new changes in files

--- script.js
+++ script.js
@@ -10,6 +10,7 @@
 // var number1=10;
 // var number2=20;
 // console.log(number1+n
+/*
 function num(rasna){
     if(rasna>18){
         console.log("do drink the rasna");
@@ -21,3 +22,16 @@ function num(rasna){
 }
 c2=num(2);
 console.log(c2);
+*/
+
+//loops
+
+// let arr=[1,2,3,4,5];
+// console.log(arr);
+// for(var i=0;i<arr.length;i++){
+//     console.log(i);
+// }
+
+function clicked(){
+    console.log("user clicked on the button");
+}
